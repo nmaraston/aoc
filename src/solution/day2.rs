559a59e8b8a0df76e0c91eee190d5c61de638ed0
nmaraston <nick.maraston@gmail.com>
2019-12-11 01:37:47 -0500

    Add day 2 part 2 solution

--- src/solution/day2.rs
+++ src/solution/day2.rs
@@ -7,39 +7,85 @@ pub struct Day2Solve {}
 impl Solution for Day2Solve {
 
     fn part_1(&self, input_file_path: &str) -> std::io::Result<String> {
-        let mut ints = read_ints(input_file_path)?;
-
-        ints[1] = 12;
-        ints[2] = 2;
-
-        let mut index: usize = 0;
-        while ints[index] != 99 {
-            let op1_register = ints[index + 1] as usize;
-            let op2_register = ints[index + 2] as usize;
-            let register = ints[index + 3] as usize;
-
-            ints[register] = match ints[index] {
-                1 => ints[op1_register] + ints[op2_register],
-                2 => ints[op1_register] * ints[op2_register],
-                _ => panic!("Unknown op code found.")
-            };
-
-            index += 4;
-        }
-
-        Ok(ints[0].to_string())
+        let mut memory = read_ints(input_file_path)?;
+        let result = compute(12, 2, &mut memory);
+        Ok(result.to_string())
     }
 
     fn part_2(&self, input_file_path: &str) -> std::io::Result<String> {
-        Ok("Unimplemented".to_owned())
+        let mut memory = read_ints(input_file_path)?;
+
+        /*
+         * Search through (noun, verb) pairs to find the answer.
+         * The larger the sum of noun + verb the larger the result will be (this is clear if you
+         * examine the first two op instructions in the input file). Therefore, we can regard the
+         * following two statements as true and leverage them to prune some pairs:
+         * 
+         *     1. Since the computed as in part 1 is less then the answer in part 2 we can skip all
+         *        pairs where noun < 12 and verb <= 2.
+         *     2. If v1 <= v2 and compute(noun, v1) > 19690720 then compute(noun, v2) >
+         *        compute(noun, v1) > 19690720 thus we can prune pairs for (noun, v) where v > v1.
+         */
+        let mut result = 0;
+        for noun in 12..99 {
+            for verb in 3..99 {
+                let mut mem_copy = memory.clone();
+                let sub_res = compute(noun, verb, &mut mem_copy);
+                if sub_res == 19690720 {
+                    result = (100 * noun) + verb;
+                } else if sub_res == 0 || sub_res > 19690720 {
+                    break;
+                }
+            }
+
+            // Exit if we found the answer
+            if result != 0 {
+                break;
+            }
+        }
+        
+        Ok(result.to_string())
     }
 }
 
-fn read_ints(input_file_path: &str) -> std::io::Result<Vec<i32>> {
-    let ints: Vec<i32> = fs::read_to_string(input_file_path)?
+/**
+ * Run the Intcode programming as documented. Return 0 if a (noun, verb) input pair causes a index
+ * out of bounds error.
+ */
+fn compute(noun: u32, verb: u32, memory: &mut Vec<u32>) -> u32 {
+
+    memory[1] = noun;
+    memory[2] = verb;
+    
+    let mut instr_ptr: usize = 0;
+
+    while memory[instr_ptr] != 99 {
+        let op1_register = memory[instr_ptr + 1] as usize;
+        let op2_register = memory[instr_ptr + 2] as usize;
+        let register = memory[instr_ptr + 3] as usize;
+
+        memory[register] = match memory[instr_ptr] {
+            1 => memory[op1_register] + memory[op2_register],
+            2 => memory[op1_register] * memory[op2_register],
+            _ => panic!("Unknown op code found.")
+        };
+
+        instr_ptr += 4;
+
+        if instr_ptr >= memory.len() {
+            return 0;
+        }
+    }
+
+    memory[0]
+}
+
+
+fn read_ints(input_file_path: &str) -> std::io::Result<Vec<u32>> {
+    let ints: Vec<u32> = fs::read_to_string(input_file_path)?
         .trim()
         .split(",")
-        .map(|s| s.parse::<i32>().unwrap())
+        .map(|s| s.parse::<u32>().unwrap())
         .collect();
     Ok(ints)
 }
