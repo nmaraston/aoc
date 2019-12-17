249ba7e79280c4041a82a2834a0fe145ee9a8356
nmaraston <nick.maraston@gmail.com>
2019-12-17 17:14:13 -0500

    Add day 4 part 2 solution

--- src/solution/day4.rs
+++ src/solution/day4.rs
@@ -9,33 +9,33 @@ impl Solution for Day4Solve {
     fn part_1(&self, input_file_path: &str) -> std::io::Result<String> {
         let range = read_range(input_file_path)?;
 
-        let mut valid_pass_count = 0;
-        for num in range.0 .. range.1 {
+        let count = count_valid_passwords(range.0, range.1, |val| {
+            let digits = digits(val);
+            mono_increasing(&digits) && has_double(&digits)
+        });
 
-            let mut has_double = false;
-            let mut mono_increasing = true;
-
-            let digits = digits(num);
-
-            for i in 1 .. digits.len() {
-                has_double = has_double || (digits[i] == digits[i - 1]);
-                mono_increasing = mono_increasing && (digits[i] <= digits[i - 1]);
-                if !mono_increasing {
-                    break;
-                }
-            }
-
-            if has_double && mono_increasing {
-                valid_pass_count += 1;
-            }
-        }
-
-        Ok(valid_pass_count.to_string())
+        Ok(count.to_string())
     }
 
     fn part_2(&self, input_file_path: &str) -> std::io::Result<String> {
-        Ok("Unimplemented".to_owned())
+        let range = read_range(input_file_path)?;
+
+        let count = count_valid_passwords(range.0, range.1, |val| {
+            let digits = digits(val);
+            mono_increasing(&digits) && has_strict_double(&digits)
+        });
+
+        Ok(count.to_string())
     }
+}
+
+fn count_valid_passwords(lower: u32, upper: u32, valid: fn(u32) -> bool) -> u32 {
+    (lower .. upper)
+        .fold(0, |sum, val| {
+            match valid(val) {
+                true => sum + 1,
+                false => sum
+            }})
 }
 
 fn digits(num: u32) -> Vec<u32> {
@@ -49,9 +49,42 @@ fn digits(num: u32) -> Vec<u32> {
     result
 }
 
+fn has_double(digits: &Vec<u32>) -> bool {
+    let mut result = false;
+    for i in 1 .. digits.len() {
+        result = result || (digits[i] == digits[i - 1]);
+    }
+    result
+}
+
+fn has_strict_double(digits: &Vec<u32>) -> bool {
+    let mut cand = digits[0];
+    let mut adj_count = 1;
+    for i in 1 .. digits.len() {
+        if digits[i] == cand {
+            adj_count += 1;
+        } else if adj_count == 2 {
+            return true;
+        } else {
+            cand = digits[i];
+            adj_count = 1;
+        }
+    }
+
+    adj_count == 2
+}
+
+fn mono_increasing(digits: &Vec<u32>) -> bool {
+    for i in 1 .. digits.len() {
+        if !(digits[i - 1] >= digits[i]) {
+            return false;
+        }
+    }
+    true
+}
 
 fn read_range(input_file_path: &str) -> std::io::Result<(u32, u32)> {
-    let contents: Vec<u32>  = fs::read_to_string(input_file_path)?
+    let contents: Vec<u32> = fs::read_to_string(input_file_path)?
         .trim()
         .split("-")
         .map(|n| n.parse::<u32>().unwrap())
