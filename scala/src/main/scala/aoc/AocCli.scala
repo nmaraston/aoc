a6b5f8f4de61398609b28db2f0ebff53b74048e5
nmaraston <nick.maraston@gmail.com>
2020-09-18 17:25:22 -0400

    Add resource file parameter to CLI and solution trait

--- scala/src/main/scala/aoc/AocCli.scala
+++ scala/src/main/scala/aoc/AocCli.scala
@@ -2,6 +2,7 @@ package aoc
 
 import java.io.PrintStream
 import java.util.concurrent.Callable
+import scala.io.Source
 
 import picocli.CommandLine.{Command, Parameters}
 
@@ -13,17 +14,24 @@ class AocCli(val printStream: PrintStream) extends Callable[Int] {
 
   @Parameters(index = "0") private var day: Int = 0
   @Parameters(index = "1") private var part: Int = 0
+  @Parameters(index = "2") private var inputFilename: String = _
 
-  private val SolutionIndex: Map[Int, Solution] = Map()
+  private val SolutionIndex: Map[Int, Solution] = Map(
+    1 -> new Day1Solution()
+  )
 
   override def call(): Int = {
     val result = (SolutionIndex.get(day), part) match {
-      case (Some(solution), 1) => solution.part1()
-      case (Some(solution), 2) => solution.part2()
-      case (None, _) => Error(f"No solution found for day=$day and part=$part")
+      case (Some(solution), 1) => solution.part1(fileSource(inputFilename))
+      case (Some(solution), 2) => solution.part2(fileSource(inputFilename))
+      case _ => Error(f"No solution found for day '$day'")
     }
 
     printStream.println(result.either())
     if (result.isOk()) 0 else 1
+  }
+
+  private def fileSource(filename: String): Source = {
+    Source.fromResource(filename)
   }
 }
--- scala/src/main/scala/aoc/Solution.scala
+++ scala/src/main/scala/aoc/Solution.scala
@@ -1,5 +1,7 @@
 package aoc
 
+import scala.io.Source
+
 trait Result {
 
   def either(): String = this match {
@@ -16,6 +18,6 @@ case class Ok(answer: String) extends Result
 case class Error(msg: String) extends Result
 
 trait Solution {
-  def part1(): Result
-  def part2(): Result
+  def part1(source: Source): Result
+  def part2(source: Source): Result
 }
